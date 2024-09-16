99f01073dac0fe8f19ed28f247bac463b97b9e5b
arslanmoosa <arslanmoosa328@gmail.com>
2024-09-16 13:43:40 +0500

    Part 2 of project by arslan ahmad

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Settings:
+    DATABASE_URL= "postgresql://amdb_a2sm_user:M9kjpfcdME1EiFgyDFCsJJX2A6IOi2pE@dpg-crjtq7l2ng1s73fkoocg-a.oregon-postgres.render.com/amdb_a2sm"
+
+settings = Settings()
